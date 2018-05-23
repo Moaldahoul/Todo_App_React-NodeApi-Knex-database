f6759e873f089f4db013db4d812c6249a8cdb7fd
Moaldahoul <ma811forever@gmail.com>
2018-05-23 11:15:52 +0200

    Deploying Todo_App to Heroku

--- nodeTodo/database/index.js
+++ nodeTodo/database/index.js
@@ -1,12 +1,16 @@
-const knex = require('knex')(process.env.NODE_ENV !== 'production' ? {
+const optings = process.env.NODE_ENV === 'production' ? 
+{
+    client: 'pg',
+    connection: process.env.DATABASE_URL, searchPath:['public']
+} : 
+ {
     client: 'pg',
     connection: {
         host: 'localhost',
         database: 'todo_db'
     }
-} : {
-    client: 'pg', connection: process.env.DATABASE_URL, searchPath: ['public']
-  }
-);
+ };
+
+const knex = require('knex')(options);
 
 module.exports = knex;
--- nodeTodo/index.js
+++ nodeTodo/index.js
@@ -12,5 +12,14 @@ app.use(bodyParser.urlencoded({
 
 app.use('/api', apiRoute);
 
+if(process.env.NODE_ENV ==='production'){
+    app.use(express.static('react_todo/build'));
+
+    const path = require('path');
+    app.get('*', function(req, res){
+        res.sendFile(path.resolve(__dirname, 'react_todo', 'build', 'index.html'))
+    })  
+}
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT);
